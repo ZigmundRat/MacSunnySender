d477db81f284a1c8950be955960504e35ed25c34
Jan Verrept <janverrept@me.com>
2017-08-07 20:16:30 +0200

    Initial Commit

---
+++ MacSunnySender/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacSunnySender
+//
+//  Created by Jan Verrept on 7/08/17.
+//  Copyright © 2017 OneClick. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
