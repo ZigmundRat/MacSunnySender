f5102d4f35a45b060ab889293d8a42da6266f1d1
Jan Verrept <janverrept@me.com>
2017-08-19 23:27:32 +0200

    Prepared for emialclient

---
+++ MacSunnySender/EmailClient.swift
@@ -0,0 +1,15 @@
+//
+//  EmailClient.swift
+//  MacSunnySender
+//
+//  Created by Jan Verrept on 19/08/17.
+//  Copyright © 2017 OneClick. All rights reserved.
+//
+
+import Foundation
+
+func sendDataToSunnyPortal(inverter:SMAinverter){
+	
+	// Yet to implement this functionality
+	
+}
