e2003eead0a955efb0036003f431b33ea5475680
Jan Verrept <janverrept@me.com>
2017-11-30 11:25:19 +0100

    DBASE working again!!!

--- MacSunnySender/MainWindow/ViewModels/SMAInverter.swift
+++ MacSunnySender/MainWindow/ViewModels/SMAInverter.swift
@@ -19,7 +19,7 @@ protocol InverterViewModel {
     var measurementValues:[Measurement]?{get}
     var parameterValues:[Measurement]?{get}
     var testValues:[Measurement]?{get}
-
+    
 }
 
 /// C-callback functions
@@ -198,7 +198,7 @@ class SMAInverter: InverterViewModel{
         
         var channelTypesToRead = [channelType]
         if channelType == .allChannels{
-            channelTypesToRead = [ChannelsType.allChannels, ChannelsType.parameterChannels, ChannelsType.testChannels]
+            channelTypesToRead = [ChannelsType.spotChannels, ChannelsType.parameterChannels, ChannelsType.testChannels]
         }
         for typeToRead in channelTypesToRead{
             
@@ -239,7 +239,7 @@ class SMAInverter: InverterViewModel{
                         var channelRecord = Channel(
                             channelID: nil,
                             inverterID: model.inverterID,
-                            type: Int(channelType.rawValue),
+                            type: Int(typeToRead.rawValue),
                             number: channelNumber,
                             name: String(cString: channelName),
                             unit: String(cString: unit)
@@ -269,7 +269,7 @@ class SMAInverter: InverterViewModel{
                     channelHandles = channelHandles.advanced(by: 1)
                 }
                 
-               
+                
                 
             }
         }
@@ -359,7 +359,7 @@ class SMAInverter: InverterViewModel{
                         var sqlRecord = JVSQliteRecord(data:measurementRecord, in:dataBaseQueue)
                         let changedSQLRecords = sqlRecord.createRecord()
                         measurementRecord.channelID = changedSQLRecords?.first?[0]
-
+                        
                         currentValues.append(measurementRecord)
                         
                     }
@@ -385,6 +385,10 @@ class SMAInverter: InverterViewModel{
     
     public func saveCsvFile(forDate dateQueried: String){
         
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeZone = TimeZone.current
+        dateFormatter.dateFormat = "dd-MM-yyyy" // Local date string
+        
         var CSVSource = """
                 SUNNY-MAIL
                 Version    1.2
@@ -397,18 +401,37 @@ class SMAInverter: InverterViewModel{
         
         let dataSeperator = ";"
         
-        if let dataRows = searchData(forDate: Date()){
-            let columNamesUsed = ["inverter.type","inverter.serial","channel.name","measurement.date","measurement.dailyvalue","measurement.startValue","measurement.endValue"]
-            var dataString:String
+        if let dailyRecords = searchData(forDate: Date()){
+        let columNamesUsed = ["inverter.type","inverter.serial","channel.name","measurement.date","measurement.dailyvalue","measurement.startValue","measurement.endValue"]
             
-            for dataRow in dataRows{
-                var dataStrings:[String] = []
+            var recordsToReport:[Row] = []
+
+            if let firstRecordedTime = dateFormatter.date(from: (dailyRecords.first!["time"])!){
+            
+                var timeToReport = firstRecordedTime
                 
-                for columnName in columNamesUsed{
-                    dataString = NSString(data: dataRow.dataNoCopy(named: columnName)!, encoding: String.Encoding.utf8.rawValue)! as String
-                    dataStrings.append(dataString)
+                for record in dailyRecords{
+                    let recordedTime = dateFormatter.date(from: (record["time"])!)
+                    
+                    if recordedTime?.compare(timeToReport) == ComparisonResult.orderedAscending{
+                        // Not there yet
+                    }else if recordedTime?.compare(timeToReport) == ComparisonResult.orderedDescending{
+                        // Shooting past the interval, point to the next higher interval
+                        while recordedTime?.compare(timeToReport) == ComparisonResult.orderedDescending{
+                            timeToReport = timeToReport.addingTimeInterval(15*60)
+                        }
+                        // and give it a second shot
+                        if recordedTime?.compare(timeToReport) == ComparisonResult.orderedSame{
+                            recordsToReport.append(record)
+                        }
+                    }else{
+                        // When it was recorded at the exact time-interval
+                        // Put the record in the report
+                        recordsToReport.append(record)
+                    }
+                    
                 }
-                CSVSource += dataStrings.joined(separator: dataSeperator)
+                
             }
         }
         print(CSVSource) // replace with saved CSVFile
@@ -431,23 +454,8 @@ class SMAInverter: InverterViewModel{
         )
         
         var dailyRequest = JVSQliteRecord(data:searchRequest, in:dataBaseQueue)
-        let dailyRecords = dailyRequest.findRecords()
         
-        if let startTime = dateFormatter.date(from: (dailyRecords?.first!["time"])!),
-        let endTime = dateFormatter.date(from: (dailyRecords?.last!["time"])!),
-        let interval = dateFormatter.date(from: "00:15:00"){
-        
-
-            var time = startTime
-            while time <= endTime{
-                
-                time = Calendar.current.date(byAdding:interval., to: time, wrappingComponents: false)
-
-            }
-        
-        }
-    
-        return dailyRecords
+        return dailyRequest.findRecords()
     }
     
     
