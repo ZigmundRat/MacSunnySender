03c70872461e352c069063b556a25e276744e88e
Jan Verrept <janverrept@me.com>
2017-12-05 18:58:10 +0100

    WIth original submodules reloaded

--- Frameworks/YASDI/build-xcode/yasdi.build/Debug/yasdimaster.build/Script-D70548B40BBD47BBB4FF3E8E.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/janverrept/Documents/Development/Projects/Personal/Xcode/MacSunnySender/Frameworks/YASDI/build-xcode -f /Users/janverrept/Documents/Development/Projects/Personal/Xcode/MacSunnySender/Frameworks/YASDI/build-xcode/CMakeScripts/yasdimaster_postBuildPhase.make$CONFIGURATION all
